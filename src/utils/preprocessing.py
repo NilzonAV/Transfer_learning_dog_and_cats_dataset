ec8a1c982e0c960d5853c08802cadc8a6bdd57ad
Nilzon <nilzon.aguirrev@gmail.com>
2024-09-18 19:36:26 -0400

    dsa

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,8 +1,9 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 import numpy as np
 import os
+import pandas as pd
 
-def create_train_generator(train_path='train', image_size=(150, 150), batch_size=20):
+def create_train_generator(train_path='dogs-vs-cats/train', image_size=(224, 224), batch_size=20):
     """
     Create and return an image data generator for the training dataset.
     
@@ -11,24 +12,43 @@ def create_train_generator(train_path='train', image_size=(150, 150), batch_size
     - image_size: Target size of images (width, height).
     - batch_size: Size of the batches of data.
     """
-    train_datagen = ImageDataGenerator(rescale=1./255)
+    # Create a DataFrame
+    data = []
+    for filename in os.listdir(train_path):
+        if filename.startswith('cat'):
+            label = 'cat'
+        elif filename.startswith('dog'):
+            label = 'dog'
+        else:
+            continue
+        data.append({'filename': filename, 'class': label})
 
-    train_generator = train_datagen.flow_from_directory(
-        train_path,
+    df = pd.DataFrame(data)
+
+    # Create ImageDataGenerator
+    datagen = ImageDataGenerator(rescale=1./255)
+
+    # Create generator using flow_from_dataframe
+    train_generator = datagen.flow_from_dataframe(
+        df,
+        directory=train_path,
+        x_col='filename',
+        y_col='class',
         target_size=image_size,
-        batch_size=batch_size,
-        class_mode='binary')
-    
+        class_mode='binary',
+        batch_size=batch_size
+    )
+
     return train_generator
 
-def load_test_images(directory, target_size=(150, 150)):
+def load_test_images(directory, target_size=(224, 224)):
     """
     Load and preprocess test images from a specified directory.
     
     Parameters:
     - directory: Path to the directory containing test images.
     - target_size: Target size of images (width, height).
-    """
+    """ 
     images = []
     filenames = []
     for fname in sorted(os.listdir(directory), key=lambda x: int(x.split('.')[0])):
