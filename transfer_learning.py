cea8abdbd665d13618e0f02b01477f28af5ff043
Nilzon <nilzon.aguirrev@gmail.com>
2024-09-18 20:14:58 -0400

    transfer

--- transfer_learning.py
+++ transfer_learning.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
 from src.utils.preprocessing import create_train_generator, load_test_images
+from tensorflow.keras.models import load_model
 
 def get_transfer_model(input_shape=(224, 224, 3)):
     base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=input_shape, pooling='avg')
@@ -23,7 +24,15 @@ train_generator = create_train_generator(train_path='src/dogs-vs-cats/train', ba
 model = get_transfer_model(input_shape=(224, 224, 3))
 
 # Train the model
-history = model.fit(train_generator, epochs=5, callbacks=[EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)])
+history = model.fit(train_generator, epochs=5, 
+                    callbacks=[EarlyStopping(monitor='loss', patience=3, restore_best_weights=True)])
+
+# Save the trained model
+model.save('model_v1.h5')
+
+# Loading the model (in a new session or script)
+model = load_model('model_v1.h5')
+
 
 # Load and predict test images
 test_images, test_filenames = load_test_images('src/dogs-vs-cats/test1')
